90b3e4aa07123a6328ba8e977cedc961c8567c93
zhdchips <1477627480@qq.com>
2022-03-07 18:49:18 +0800

    github上第一次提交

---
+++ src/test/java/com/example/gittestongithub/GittestOnGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gittestongithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GittestOnGithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
